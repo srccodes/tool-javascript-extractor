657186cbca45ce5792ca4f90244746ad2221e8cd
SrcCodes <abhijit.docs@gmail.com>
2013-08-09 18:50:09 +0530

    Update JavaScriptExtractor.java

--- src/main/java/com/srccodes/tools/html/extractor/JavaScriptExtractor.java
+++ src/main/java/com/srccodes/tools/html/extractor/JavaScriptExtractor.java
@@ -16,7 +16,7 @@ public class JavaScriptExtractor {
 		OutputSettings outputSettings = new OutputSettings();
 		outputSettings.prettyPrint(true);
 		
-		Document doc = Jsoup.connect("http://srccodes.com")
+		Document doc = Jsoup.connect("http://wikimapia.org")
 				.userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/536.30.1 (KHTML, like Gecko) Version/6.0.5 Safari/536.30.1")
 				.followRedirects(true)
 				.get().outputSettings(outputSettings);
